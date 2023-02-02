242457e24fa0a0ea3d6f224acd0ab7328272f961
noracamacho <ingnorac@gmail.com>
2023-02-01 22:53:31 -0600

    Adding navigation Logout

--- src/components/shared/AppNavBar.jsx
+++ src/components/shared/AppNavBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import './styles/appNavBar.css'
 
 const AppNavBar = () => {
+
+    const [isLogged, setIsLogged] = useState(false);
+    useEffect(() => {
+        const condition = localStorage.getItem('token') ? true : false;
+        setIsLogged(condition);
+    }, [])
+
   return (
     <Navbar bg="primary" expand="lg" variant='dark'>
       <Container>
@@ -14,9 +21,9 @@ const AppNavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link as={Link} to="/purchases">Purchases</Nav.Link>
             <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+            <Nav.Link as={Link} to="/login">{isLogged ? 'Logout' : 'Login'}</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
